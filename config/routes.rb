63fd92d915eeac5c2d1dbfe90ecf10cc29cea6b0
shwetapangam <shwetasingh231@gmail.com>
2021-04-02 12:51:01 +0530

    Implement login through github auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 end
