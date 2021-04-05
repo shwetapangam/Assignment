0d057ae5f3fef17801817b2f652d63921473b38c
shwetapangam <shwetasingh231@gmail.com>
2021-04-05 14:25:11 +0530

    Display github followers count and line indentation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,33 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import axios from "axios"
+require("jquery")
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+$( document ).on('turbolinks:load', function() {
+
+	const user = $("#current_user_name").val();
+
+	axios({
+		method: 'get',
+		url: 'https://api.github.com/users/'+user+'/followers'
+	})
+	.then(function (response) {
+		$("#git_follower_count").append(response.data.length)
+
+	});
+
+	axios({
+		method: 'get',
+		url: 'https://api.github.com/'+user+'/shwetapangam/repos'
+	})
+	.then(function (response) {
+		$("#git_repo_count").append(response.data.length)
+	});
+})
